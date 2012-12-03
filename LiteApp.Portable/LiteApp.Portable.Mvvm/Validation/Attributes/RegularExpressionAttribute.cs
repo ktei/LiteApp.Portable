86e1668b135fdf26360fbeb323d42346224bce00
Rui Cheng <ktei2008@gmail.com>
2012-12-03 15:40:46 +1100

    validation framework

---
+++ LiteApp.Portable/LiteApp.Portable.Mvvm/Validation/Attributes/RegularExpressionAttribute.cs
@@ -0,0 +1,18 @@
+﻿
+namespace LiteApp.Portable.Mvvm.Validation
+{
+    public class RegularExpressionAttribute : ValidationAttribute
+    {
+        public RegularExpressionAttribute(string pattern)
+        {
+            Pattern = pattern;
+        }
+
+        public override string DefaultErrorMessage
+        {
+            get { return "Pattern is wrong."; }
+        }
+
+        public string Pattern { get; private set; }
+    }
+}
---
+++ LiteApp.Portable/LiteApp.Portable.Mvvm/Validation/ValidationRuleFactory.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace LiteApp.Portable.Mvvm.Validation
+{
+    public static class ValidationRuleFactory
+    {
+        static IRuleLocator _locator;
+
+        public static void Initialize(IRuleLocator locator)
+        {
+            if (locator == null)
+                throw new ArgumentNullException("locator");
+            if (_locator != null)
+                throw new NotSupportedException("Rule locator has already been set.");
+            _locator = locator;
+        }
+
+        public static IValidationRule GetRule(ValidationAttribute attr)
+        {
+            if (_locator == null)
+                throw new NotSupportedException("Rule locator is needed. Call Initialize to provide a locator first.");
+            return _locator.FindRule(attr);
+        }
+    }
+}
